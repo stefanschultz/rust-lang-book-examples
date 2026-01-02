1315602b83011b4293bf4c778d48d6180677f1f0
stefanschultz <info@schultzsoftware.com>
2026-01-02 12:56:55 +0100

    add project structs_i

---
+++ structs_i/src/main.rs
@@ -0,0 +1,61 @@
+// struct
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+// tuple structs
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+// unit-like structs (also "unit struct") like as unit type ()
+struct AlwaysEqual;
+
+fn main() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    /*let user2 = User {
+        active: user1.active,
+        username: user1.username,
+        email: String::from("another@example.com"),
+        sign_in_count: user1.sign_in_count,
+    };*/
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+    // destruct
+    let Point(x, y, z) = origin;
+
+    let subject = AlwaysEqual;
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
+}
+
+fn build_user_shorthand(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
